330d90b67c32303447e35e71c96698ff278359cb
harshitxrajput <harshitxrajput@gmail.com>
2025-08-29 20:51:17 +0530

    message and conversation model created

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
     participants: [{
-        participantId: { type: mongoose.Schema.Types.ObjectId, refPath: "userModel", required: true },
+        participantId: { type: mongoose.Schema.Types.ObjectId, refPath: "participants.participantModel", required: true },
         participantModel: { type: String, enum: ["Tenant", "Landlord"], required: true }
     }],
 
@@ -23,3 +23,42 @@ const conversationSchema = new mongoose.Schema({
         ref: "Message"
     }
 }, { timestamps: true });
+
+const conversationModel = mongoose.model("Conversation", conversationSchema);
+
+const messageSchema = new mongoose.Schema({
+    conversationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Conversation",
+        required: true
+    },
+
+    sender: {
+        senderId: { type: mongoose.Schema.Types.ObjectId, required: true, refPath: "sender.senderModel" },
+        senderModel: { type: String, required: true, enum: ["Tenant", "Landlord"] }
+    },
+
+    messageType: {
+        type: String,
+        enum: ["text", "image", "file"],
+        default: "text"
+    },
+
+    text: {
+        type: String,
+        trim: true
+    },
+
+    mediaUrl: {
+        type: String
+    },
+
+    isRead: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+const messageModel = mongoose.model("Message", messageSchema);
+
+export {conversationModel, messageModel};
